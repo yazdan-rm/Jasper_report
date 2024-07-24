4f64da5181daa6c3b8afffe76eeac9fefa37e9a7
Yazdan <116789371+yazdan-rm@users.noreply.github.com>
2024-07-24 08:51:49 +0330

    Update report.sql

--- report.sql
+++ report.sql
@@ -1,67 +1,94 @@
--- auto-generated definition
-create table MEM$MEMBERSHIP
-(
-    ID                                NUMBER(19) not null
-        primary key,
-    IP                                VARCHAR2(255 char),
-    VERSION                           NUMBER(19),
-    CODING                            VARCHAR2(255 char),
-    CREATED_BY                        NUMBER(19),
-    CREATED_DATE                      TIMESTAMP(6),
-    DELETED_BY                        NUMBER(19),
-    DELETED_DATE                      TIMESTAMP(6),
-    DESCRIPTION                       VARCHAR2(255 char),
-    GROUP_TAG                         VARCHAR2(255 char),
-    IS_CONFIRMED                      NUMBER(1),
-    IS_DELETED                        NUMBER(1)  not null,
-    FINAL_FLAG                        NUMBER(1),
-    IS_LOCKED                         NUMBER(1),
-    IS_SECURE                         NUMBER(1),
-    STAR_FLAG                         NUMBER(1),
-    IS_TEMP_DELETED                   NUMBER(1),
-    LEVEL_FLAG                        NUMBER(10),
-    PERMISSION_FLAG                   NUMBER(10),
-    FK_PV_STEREO_TYPE_ID              NUMBER(19),
-    REQUEST_ID                        NUMBER(19),
-    ROW_LEVEL_ID                      NUMBER(19),
-    FK_SYS_SCOPE_ID                   NUMBER(19),
-    SYSTEM_TAG                        VARCHAR2(255 char),
-    UPDATED_BY                        NUMBER(19),
-    UPDATED_DATE                      TIMESTAMP(6),
-    USER_TAG                          VARCHAR2(255 char),
-    ZONE_BRANCH_ID                    NUMBER(19),
-    ZONE_CHART_ID                     NUMBER(19),
-    ZONE_CODE                         VARCHAR2(255 char),
-    ZONE_ORG_ID                       NUMBER(19),
-    ZONE_POST_ID                      NUMBER(19),
-    END_DATE                          DATE,
-    IS_ARCHIVED                       NUMBER(1),
-    IS_SHARED_FILE                    NUMBER(1),
-    FK_ORG_CORTEX_ID                  NUMBER(19),
-    FK_ORG_ID                         NUMBER(19),
-    FK_PV_ARCHIVE_TYPE_ID             NUMBER(19),
-    FK_PV_MEM_STATUS_ID               NUMBER(19),
-    FK_PV_MEMBERSHIP_TYPE_ID          NUMBER(19),
-    START_DATE                        DATE,
-    FK_MEM_TYPE_LEVEL_ID              NUMBER(19)
-        constraint FK7BR0UYSTLR8QR3RRQ7XGPLLAXa
-            references BI$MEMBERSHIP_TYPE_LEVEL,
-    FK_PERSON_ID                      NUMBER(19)
-        constraint FK2DWVATXWF5WJE9EN3LPNLBDNB
-            references PSN$PERSON,
-    PRIORITY                          NUMBER(10),
-    IS_LAST_MEM                       NUMBER(1),
-    INITIAL_START_DATE                DATE,
-    IS_FIRST_MEM                      NUMBER(1),
-    IS_MEM_STATUS_ID_CHANGED          NUMBER(1),
-    FK_PV_DOWNGRADE_TYPE_ID           NUMBER(19),
-    FK_PV_DOWNGRADE_CATEGORY_ID       NUMBER(19),
-    DOWNGRADE_REASON_DESCRIPTION      VARCHAR2(255 char),
-    FK_PV_DOWNGRADE_REASON_ID         NUMBER(19),
-    FILE_NUMBER                       VARCHAR2(255 char),
-    FK_MEM_CORTEX_DOWNGRADE_REASON_ID NUMBER(19)
-        constraint FKCXFEXGP8GEECPM9I58W8JSF1L
-            references MEM$CORTEX_DOWNGRADE_REASON
-)
-/
+package ir.rbp.nab.model.domainmodel.membership;
 
+import ir.rbp.nab.model.domainmodel.person.PSNPerson;
+import ir.rbp.nab.model.domainmodel.setting.bi.BIMembershipTypeLevel;
+import ir.rbp.nabcore.model.domainmodel.BaseEntity;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.apache.commons.lang3.builder.HashCodeExclude;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serial;
+import java.time.LocalDate;
+
+@EqualsAndHashCode(callSuper = true, doNotUseGetters = true)
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "MEM$MEMBERSHIP")//,uniqueConstraints = @UniqueConstraint(columnNames = {"START_DATE","FK_ORG_ID","FK_PV_MEMBERSHIP_TYPE_ID","FK_PERSON_ID"}))
+@SequenceGenerator(initialValue = 100, name = "SEQ_GENERATOR", sequenceName = "MEM$MEMBERSHIP_SEQ", allocationSize = 1)
+public class MEMMembership extends BaseEntity {
+
+    @Serial
+    private static final long serialVersionUID = -4920436770452366817L;
+
+    @NotNull
+    @Column(name = "START_DATE", nullable = false)
+    private LocalDate startDate;
+
+    @Column(name = "END_DATE")
+    private LocalDate endDate;
+
+    @Column(name = "IS_SHARED_FILE")
+    private Boolean isSharedFile;
+
+    @NotNull
+    @Column(name = "IS_ARCHIVED", nullable = false)
+    private Boolean isArchived;
+
+    @NotNull
+    @Column(name = "FK_ORG_ID", nullable = false)
+    private Long orgId;
+
+    @Column(name = "FK_ORG_CORTEX_ID")
+    private Long orgCortexId;
+
+    @Column(name = "FK_PV_MEMBERSHIP_TYPE_ID")
+    private Long pvMembershipTypeId;
+
+    @Column(name = "FK_PV_ARCHIVE_TYPE_ID")
+    private Long pvArchiveTypeId;
+
+    @Column(name = "FK_PV_DOWNGRADE_CATEGORY_ID")
+    private Long pvDowngradeCategoryId;
+
+    @Column(name = "IS_LAST_MEM")
+    private Boolean isLastMembership;
+
+    @Column(name = "FK_PV_DOWNGRADE_REASON_ID")
+    private Long pvDowngradeReasonId;
+
+    @Column(name ="DOWNGRADE_REASON_DESCRIPTION")
+    private String downgradeReasonDescription;
+
+    @Column(name = "FILE_NUMBER")
+    private String fileNumber;
+    /**
+     * indicates that the membership is the first membership in the org or not,
+     * in new persist and new subscription it must be set to true
+     */
+    @Column(name = "IS_FIRST_MEM")
+    private Boolean isFirstMembership;
+
+    @NotNull
+    @Column(name = "FK_PV_MEM_STATUS_ID", nullable = false)
+    private Long pvMemStatusId;
+
+    @Column(name = "IS_MEM_STATUS_ID_CHANGED")
+    private Boolean isMemStatusIdChanged;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "FK_MEM_TYPE_LEVEL_ID")
+    @HashCodeExclude
+    private BIMembershipTypeLevel membershipTypeLevel;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST}, optional = false)
+    @JoinColumn(name = "FK_PERSON_ID", nullable = false)
+    private PSNPerson person;
+
+}
