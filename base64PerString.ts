f815660a9143925e5e5b43e25a6e0ea6f659e482
Yazdan <116789371+yazdan-rm@users.noreply.github.com>
2024-12-18 12:18:26 +0330

    Create base64PerString.ts

---
+++ base64PerString.ts
@@ -0,0 +1,8 @@
+export enum EN_Base64PreString {
+    PDF = 'data:application/pdf;base64,',
+    XLSX = 'data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,',
+    PNG = 'data:image/png;base64,',
+    JPG = 'data:image/jpg;base64,',
+    CSV = 'data:application/csv;base64,'
+
+}
